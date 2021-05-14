25eb3167098ebd8108fcaeadf249efa253c67070
Julian Panucci <julian.panucci@underarmour.com>
2021-05-14 11:37:25 -0500

    More

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-		.package(name: "BaseLib", path: "/Users/julian/BaseLib")
+		.package(url: "https://github.com/julpanucci/BaseLib.git", .branch("main"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
